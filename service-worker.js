6c1c59cc5bbb2b216992742ba3bec7be10ff5e0b
hyukjun._.ps <yyhhjj0715@gmail.com>
2026-08-22 11:02:13 +0900

    Refresh universal game sharing scripts

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v59";
+const CACHE_VERSION = "v60";
 const CACHE_NAME = `todaypoongsan-${CACHE_VERSION}`;
 
 const APP_SHELL = [
@@ -43,10 +43,10 @@ function withInjectedScripts(response, requestUrl) {
       if (!html.includes("game-hearts.js")) tags.push('<script src="./game-hearts.js?v=20260820-1" defer></script>');
       if (!html.includes("game-heart-retries.js")) tags.push('<script src="./game-heart-retries.js?v=20260817" defer></script>');
       if (!html.includes("game-heart-purchase.js")) tags.push('<script src="./game-heart-purchase.js?v=20260820-1" defer></script>');
-      if (!html.includes("game-extra.js")) tags.push('<script src="./game-extra.js?v=20260819-1" defer></script>');
-      if (!html.includes("opoong-run.js")) tags.push('<script src="./opoong-run.js?v=20260822-1" defer></script>');
+      if (!html.includes("game-extra.js")) tags.push('<script src="./game-extra.js?v=20260822-2" defer></script>');
+      if (!html.includes("opoong-run.js")) tags.push('<script src="./opoong-run.js?v=20260822-2" defer></script>');
       if (!html.includes("opoong-ramen.js")) tags.push('<script src="./opoong-ramen.js?v=20260822-2" defer></script>');
-      if (!html.includes("game-result-share.js")) tags.push('<script src="./game-result-share.js?v=20260822-1" defer></script>');
+      if (!html.includes("game-result-share.js")) tags.push('<script src="./game-result-share.js?v=20260822-2" defer></script>');
       if (!html.includes("stack-tuning.js")) tags.push('<script src="./stack-tuning.js?v=20260820-2" defer></script>');
       if (!html.includes("notice-override.js")) tags.push('<script src="./notice-override.js?v=20260817" defer></script>');
       if (!html.includes("qr-menu.js")) tags.push('<script src="./qr-menu.js?v=20260819-5" defer></script>');
