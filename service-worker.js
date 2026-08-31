077b5287eab6c9981ead325ee0f9c8520d52d2a8
hyukjun._.ps <yyhhjj0715@gmail.com>
2026-08-31 09:09:22 +0900

    Cache avatar gacha and refresh shop loader

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v72";
+const CACHE_VERSION = "v73";
 const CACHE_NAME = `todaypoongsan-${CACHE_VERSION}`;
 const PRESERVED_CACHE_PREFIXES = ["opoong-offline-dictionary-"];
 
@@ -37,6 +37,7 @@ const APP_SHELL = [
   "./qr-menu.js",
   "./hwp-beta.js",
   "./point-shop-fix.js",
+  "./opoong-avatar-gacha.js",
   "./memo-classroom.js",
   "./offline-dictionary.js",
   "./tools-image.js",
@@ -89,7 +90,7 @@ function withInjectedScripts(response, requestUrl) {
       if (!html.includes("notice-override.js")) tags.push('<script src="./notice-override.js?v=20260817" defer></script>');
       if (!html.includes("qr-menu.js")) tags.push('<script src="./qr-menu.js?v=20260819-5" defer></script>');
       if (!html.includes("hwp-beta.js")) tags.push('<script src="./hwp-beta.js?v=20260820-1" defer></script>');
-      if (!html.includes("point-shop-fix.js")) tags.push('<script src="./point-shop-fix.js?v=20260820-1" defer></script>');
+      if (!html.includes("point-shop-fix.js")) tags.push('<script src="./point-shop-fix.js?v=20260831-1" defer></script>');
       if (!html.includes("memo-classroom.js")) tags.push('<script src="./memo-classroom.js?v=20260822-2" defer></script>');
       if (!html.includes("offline-dictionary.js")) tags.push('<script src="./offline-dictionary.js?v=20260823-1" defer></script>');
     } else {
@@ -193,6 +194,7 @@ self.addEventListener("fetch", (event) => {
     "/qr-menu.js",
     "/hwp-beta.js",
     "/point-shop-fix.js",
+    "/opoong-avatar-gacha.js",
     "/memo-classroom.js",
     "/offline-dictionary.js",
     "/tools-image.js",
